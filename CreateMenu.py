d93a90ff01ef4eba92691dc683ba51dcacd6dad3
SERENO Mickael <serenomickael@gmail.com>
2014-01-31 13:23:18 +0100

    Complete SetTable function

--- CreateMenu.py
+++ CreateMenu.py
@@ -1,6 +1,7 @@
-from gi.repository import Gtk, Gdk
+from gi.repository import Gtk, Gdk, GdkPixbuf
 import globalVar
 import sfml as sf
+import functions
 
 class CreateMenu:
     def __init__(self, parent):
@@ -18,12 +19,10 @@ class CreateMenu:
         nameLabel = Gtk.Label("Name")
         formatLabel = Gtk.Label("Tile Format")
         numberCases = Gtk.Label("Number Cases")
-        imageLabel = Gtk.Label("Image")
 
         grid.attach(nameLabel, 0, 0, 1, 1)
         grid.attach_next_to(formatLabel, nameLabel, Gtk.PositionType.BOTTOM, 1, 1)
         grid.attach_next_to(numberCases, formatLabel, Gtk.PositionType.BOTTOM, 1, 1)
-        grid.attach_next_to(imageLabel, numberCases, Gtk.PositionType.BOTTOM, 1, 1)
 
         nameEntered = Gtk.Entry()
         nameEntered.set_text("Tile")
@@ -44,11 +43,6 @@ class CreateMenu:
         spinCasesRight = Gtk.SpinButton()
         spinCasesRight.set_adjustment(adjustment4)
 
-        imageEntered = Gtk.Entry()
-        imageEntered.set_icon_from_stock(Gtk.EntryIconPosition.SECONDARY, Gtk.STOCK_FILE)
-        imageEntered.set_icon_activatable(Gtk.EntryIconPosition.SECONDARY, True)
-        imageEntered.connect("icon-press", self.openImage, fileManager)
-
         grid.attach_next_to(nameEntered, nameLabel, Gtk.PositionType.RIGHT, 3, 1)
 
         grid.attach_next_to(spinTileLeft, formatLabel, Gtk.PositionType.RIGHT, 1, 1)
@@ -58,8 +52,6 @@ class CreateMenu:
         grid.attach_next_to(spinCasesLeft, numberCases, Gtk.PositionType.RIGHT, 1, 1)
         grid.attach_next_to(xBisLabel, spinCasesLeft, Gtk.PositionType.RIGHT, 1, 1)
         grid.attach_next_to(spinCasesRight, xBisLabel, Gtk.PositionType.RIGHT, 1, 1)
-
-        grid.attach_next_to(imageEntered, imageLabel, Gtk.PositionType.RIGHT, 3, 1)
 
         vgrid.add(grid)
         hbox = Gtk.Box()
@@ -72,8 +64,7 @@ class CreateMenu:
         buttonOK.connect("clicked", self.initNewFile, \
                 {'hTileSize':spinTileLeft, 'vTileSize':spinTileRight, \
                 'hNumberCases':spinCasesLeft, 'vNumberCases':spinCasesRight, \
-                'nameEntered':nameEntered, 'imageEntered':imageEntered, \
-                'window':window})
+                'nameEntered':nameEntered, 'window':window})
 
         buttonCancel = Gtk.Button(label="Cancel")
         buttonCancel.add_accelerator("activate", accelGroup, Gdk.KEY_Escape, 0, \
@@ -264,6 +255,7 @@ class CreateMenu:
 
         widgets = dict()
         widgets['table'] = self.newTable()
+        widgets['treeStore'] = widgets['table'].get_model()
 
         fileLabel = Gtk.Label("File")
 
@@ -273,7 +265,7 @@ class CreateMenu:
         imageEntered.connect("icon-press", self.openImage, fileManager)
         grid.attach(fileLabel, 0, 0, 1, 1)
         grid.attach_next_to(imageEntered, fileLabel, Gtk.PositionType.RIGHT, 3, 1)
-        widgets['path'] = imageEntered.get_text()
+        widgets['imageEntered'] = imageEntered
 
         hbox = Gtk.Box()
         setTableSize = Gtk.Button(label="Set Table")
@@ -301,24 +293,21 @@ class CreateMenu:
         return vgrid
 
     def newTable(self):
-        treeScore = Gtk.TreeStore(int, str, int, int, int, int)
+        treeScore = Gtk.TreeStore(str, str, str, str, str)
         treeView = Gtk.TreeView(model=treeScore)
 
-        numberRenderer = Gtk.CellRendererText() 
         titleRenderer = Gtk.CellRendererText()
         posXRenderer= Gtk.CellRendererText() 
         posYRenderer= Gtk.CellRendererText() 
         sizeXRenderer= Gtk.CellRendererText() 
         sizeYRenderer= Gtk.CellRendererText() 
 
-        columnTitle = Gtk.TreeViewColumn("Name", titleRenderer, text=1)
-        columnNumber = Gtk.TreeViewColumn("Number", numberRenderer, text=0)
-        columnPosX = Gtk.TreeViewColumn("Position x", posXRenderer, text=2)
-        columnPosY = Gtk.TreeViewColumn("Position y", posYRenderer, text=3)
-        columnSizeX = Gtk.TreeViewColumn("Size x", sizeXRenderer, text=4)
-        columnSizeY = Gtk.TreeViewColumn("Size y", sizeYRenderer, text=5)
+        columnTitle = Gtk.TreeViewColumn("Name", titleRenderer, text=0)
+        columnPosX = Gtk.TreeViewColumn("Position x", posXRenderer, text=1)
+        columnPosY = Gtk.TreeViewColumn("Position y", posYRenderer, text=2)
+        columnSizeX = Gtk.TreeViewColumn("Size x", sizeXRenderer, text=3)
+        columnSizeY = Gtk.TreeViewColumn("Size y", sizeYRenderer, text=4)
 
-        treeView.append_column(columnNumber)
         treeView.append_column(columnTitle)
         treeView.append_column(columnPosX)
         treeView.append_column(columnPosY)
@@ -328,15 +317,132 @@ class CreateMenu:
         return treeView
 
     def setTable(self, button, widgets):
-        oldWidgets = widgets
-        treeView = self.newTable()
+        if not widgets['imageEntered'].get_text():
+            return
         window = Gtk.Window(title="New image")
+        widgets['table'].set_model(widgets['treeStore'])
+        treeCopy = functions.copyTreeStore(widgets['table'].get_model())
+        widgets['treeCopied'] = treeCopy
+        widgets['window'] = window
+
+        hBox = Gtk.Box(spacing=6,orientation=Gtk.Orientation.HORIZONTAL)
+        hBox.pack_start(widgets['table'], True, True, 0)
+        hBox.pack_start(self.insertNewAnnimation(widgets), False, False, 0)
+
+        vBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        vBox.pack_start(hBox, True, True, 0)
+        vBox.pack_start(Gtk.Separator(orientation=Gtk.Orientation.HORIZONTAL), False, False, 0)
+        vBox.pack_start(self.actionAnnimation(widgets), False, False, 0)
+
         window.set_property("modal",True)
-        window.add(widgets['table'])
+        window.add(vBox)
         window.show_all()
 
+    def insertNewAnnimation(self, widgets):
+        if not widgets['imageEntered'].get_text():
+            return
+        grid = Gtk.Grid()
+        vGrid = Gtk.Grid(orientation=Gtk.Orientation.VERTICAL)
+
+        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+
+        vGrid.add(grid)
+        vGrid.add(hbox)
+
+        imagePixbuf = GdkPixbuf.Pixbuf.new_from_file(widgets['imageEntered'].get_text())
+        nameLabel = Gtk.Label("Name")
+
+        titleLabel = Gtk.Label()
+        xLabel = Gtk.Label("Position X")
+        yLabel = Gtk.Label("Position Y")
+        sizeXLabel = Gtk.Label("Size X")
+        sizeYLabel = Gtk.Label("Size Y")
+
+        nameEntry = Gtk.Entry()
+        positionXAdjustment = Gtk.Adjustment(0, 0, imagePixbuf.get_width(), 1, 10, 0) 
+        positionYAdjustment = Gtk.Adjustment(0, 0, imagePixbuf.get_height(), 1, 10, 0) 
+        sizeXAdjustment = Gtk.Adjustment(0, 0, imagePixbuf.get_width(), 1, 10, 0) 
+        sizeYAdjustment = Gtk.Adjustment(0, 0, imagePixbuf.get_height(), 1, 10, 0) 
+        xSpin = Gtk.SpinButton(adjustment=positionXAdjustment)
+        ySpin = Gtk.SpinButton(adjustment=positionYAdjustment)
+        sizexSpin = Gtk.SpinButton(adjustment=sizeXAdjustment)
+        sizeySpin = Gtk.SpinButton(adjustment=sizeYAdjustment)
+
+        addAnnim = Gtk.Button(label="Add")
+        addEntity = Gtk.Button(label="Add")
+        resetEntity = Gtk.Button(label="Reset")
+
+        separator = Gtk.Separator(orientation=Gtk.Orientation.HORIZONTAL)
+
+        grid.attach(nameLabel, 0,0,1,1)
+        grid.attach_next_to(nameEntry, nameLabel, Gtk.PositionType.RIGHT, 1, 1)
+        grid.attach_next_to(addAnnim, nameEntry, Gtk.PositionType.RIGHT, 1, 1)
+        grid.attach_next_to(separator, nameLabel, Gtk.PositionType.BOTTOM, 3, 1)
+        grid.attach_next_to(titleLabel, separator, Gtk.PositionType.BOTTOM, 4, 1)
+        grid.attach_next_to(xLabel, titleLabel, Gtk.PositionType.BOTTOM, 1, 1)
+        grid.attach_next_to(xSpin, xLabel, Gtk.PositionType.RIGHT, 2, 1)
+        grid.attach_next_to(yLabel, xLabel, Gtk.PositionType.BOTTOM, 1, 1)
+        grid.attach_next_to(ySpin, yLabel, Gtk.PositionType.RIGHT, 2, 1)
+
+        grid.attach_next_to(sizeXLabel, yLabel, Gtk.PositionType.BOTTOM, 1, 1)
+        grid.attach_next_to(sizexSpin, sizeXLabel, Gtk.PositionType.RIGHT, 2, 1)
+        grid.attach_next_to(sizeYLabel, sizeXLabel, Gtk.PositionType.BOTTOM, 1, 1)
+        grid.attach_next_to(sizeySpin, sizeYLabel, Gtk.PositionType.RIGHT, 2, 1)
+
+        hbox.pack_start(addEntity, False, False, 0)
+        hbox.pack_start(resetEntity, False, False, 0)
+        hbox.set_halign(Gtk.Align.END)
+        hbox.set_valign(Gtk.Align.END)
+
+
+        widgets['nameAnnimEntered'] = nameEntry
+        addAnnim.connect('clicked', self.addAnniInTree, widgets)
+
+        return vGrid
+
+    def addAnniInTree(self, button, widgets):
+        parent=widgets['treeStore'].append(None)
+        widgets['treeStore'].set_value(parent, 0, widgets['nameAnnimEntered'].get_text())
+        
     def cutAnnimation(self, button):
         pass
+
+    def actionAnnimation(self, widgets):
+        box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        actionBox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        delBox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+
+        actionBox.set_halign(Gtk.Align.END)
+        actionBox.set_valign(Gtk.Align.END)
+        delBox.set_valign(Gtk.Align.END)
+        delBox.set_halign(Gtk.Align.START)
+        box.set_valign(Gtk.Align.END)
+
+        box.pack_start(delBox, False, False, 0)
+        box.pack_start(actionBox, False, False, 0)
+
+        okButton = Gtk.Button(label="OK")
+        cancelButton = Gtk.Button(label="Cancel")
+        applyButton = Gtk.Button(label="Apply")
+
+        clearAll = Gtk.Button(label="Clear")
+        delete = Gtk.Button(label="Delete")
+
+        delBox.pack_start(delete, False, False, 0)
+        delBox.pack_start(clearAll, False, False, 0)
+
+        actionBox.pack_start(applyButton, False, False, 0)
+        actionBox.pack_start(okButton, False, False, 0)
+        actionBox.pack_start(cancelButton, False, False, 0)
+
+        cancelButton.connect('clicked', self.cancelAnnimation, widgets)
+
+        return box
+
+    def cancelAnnimation(self, button, widgets):
+        widgets['treeStore'] = widgets['treeCopied']
+        widgets['table'].set_model(widgets['treeStore'])
+        self.quitWindow(button, widgets['window'])
 
     def newAnnimation(self, button, widgets):
         print(widgets['xComboBox'].get_active())
@@ -374,6 +480,4 @@ class CreateMenu:
                 widgets['nameEntered'].get_text().replace(".xml", ""))
         window = widgets['window']
         del widgets['window']
-        if widgets['imageEntered'].get_text() :
-            self.newTileSet(widgets)
         window.destroy()
